a887aa31eac2ff833285f08f720c0aa496e59d88
Ray Ortiz <oray@bgsu.edu>
2023-02-27 07:47:21 -0500

    include CID info for my image.

--- ipfs/upload.js
+++ ipfs/upload.js
@@ -22,7 +22,7 @@ async function run() {
             }
             ],
             // update the IPFS CID to be your image CID
-            image: "https://ipfs.io/ipfs/QmQ2wnwaFJ1w42UTywTWpM8RgiqrWwKFR6AMrpyiHPgi3p",
+            image: "https://ipfs.io/ipfs/Qmbs3y2myzSFKRFezA5y4PWomvvN1qw7dVsicZkgh8TNrx",
             description: "So much PLW3!"
         })
     };
